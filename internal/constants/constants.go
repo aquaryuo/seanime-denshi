55f062a2ca265e748835738fdbba0555b4e048a3
Aqua <aqua@ryuo.to>
2026-06-27 07:55:52 +0000

    Bump version to 3.8.8-beta

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -6,7 +6,7 @@ import (
 )
 
 const (
-	Version              = "3.8.7"
+	Version              = "3.8.8-beta"
 	VersionName          = "Kanata"
 	GcTime               = time.Minute * 30
 	ConfigFileName       = "config.toml"
